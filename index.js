5f3ffc6d67ffd55a93387e237abc4e45c63600da
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-08-18 07:34:48 -0700

    "made changes " implant  Socket.io " update Like Follow Comment Notifications "

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRouter = require("./routes/userRouter");
@@ -9,24 +10,40 @@ const notificationRouter = require("./routes/notificationRouter");
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const port = process.env.port;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const port = process.env.port || 4000;
 
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
+// Initialize routers after io is created
 app.use("/posts", postRouter);
 app.use("/user-data", userDataRouter);
 app.use("/auth", userRouter);
-app.use('/notifications', notificationRouter);
-
-
+app.use('/notifications', notificationRouter(io));
 
 mongoose.connect(process.env.DB_URL)
   .then(() => {
-    console.log("Connected To MongoDB"),
-      app.listen(port, () => {
-        console.log("Server running on localhost:" + port);
-      });
+    console.log("Connected To MongoDB");
+    server.listen(port, () => {
+      console.log("Server running on localhost:" + port);
+    });
   })
-  .catch((err) => console.log(`unable to connect with dB : ${err.stack} `));
+  .catch((err) => console.log(`Unable to connect with DB: ${err.stack}`));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('sendNotification', (data) => {
+        const { userId, message } = data;
+        io.to(userId).emit('receiveNotification', { message });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
