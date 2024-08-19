dffce39fb5591a47a5ef825efc03757e88391549
Hamid <hamidjaved615@gmail.com>
2024-08-19 10:51:30 +0500

    Fix error

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -3,13 +3,6 @@ const router = express.Router();
 const notificationController = require('../controller/notificationControl');
 const {verifyUserToken} = require('../middleware/authUser')
 
-router.post('/add', verifyUserToken, notificationController.addNotification);
-router.get('/get-all-notifications', verifyUserToken, notificationController.getNotifications);
-router.patch('/:notificationId/mark-as-read',verifyUserToken,  notificationController.markAsRead);
-router.delete('/:notificationId/delete',verifyUserToken,  notificationController.deleteNotification);
-router.patch('/mark-all-as-read',verifyUserToken, notificationController.markAllAsRead);
-router.get('/unread-count',verifyUserToken, notificationController.getUnreadCount);
-router.get('/filter',verifyUserToken, notificationController.filterNotifications);
 
     router.post('/add', verifyUserToken, (req, res) => {
         notificationController.addNotification(req, res, io);
